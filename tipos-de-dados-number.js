8e9abfd0329b215a87de7922d73e253b0e882acc
julia vic <lucas.mata.dev@gmail.com>
2026-06-01 07:30:24 -0300

    add tipos de dados number

---
+++ tipos-de-dados-number.js
@@ -0,0 +1,7 @@
+let idade = 25; // numero intero
+let altura =1.75 // numero pontto flutuante
+let temperatura = -5 // numero negativo
+
+console.log(idade)
+console.log(altura)
+console.log(temperatura)
