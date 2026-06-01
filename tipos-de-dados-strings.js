7581054e85b43b40cdcbf91963be998f93906e6d
julia vic <lucas.mata.dev@gmail.com>
2026-06-01 07:55:45 -0300

    add cocatenação com strings

--- tipos-de-dados-strings.js
+++ tipos-de-dados-strings.js
@@ -1,7 +1,11 @@
 let nome = "ana"; // strings com aspas duplas 
 let sobrenome ='silva'; // strings com aspas simplis
-let saudacao = `ola, ${nome}!`; // template literal com interpolação de variaveis  
+let saudacao = `ola, ${nome}!`; // template literal com interpolação de variaveis 
+let nomecompleto = `${nome} ${sobrenome}!` // strings de nome completo
+
 
 console.log(nome)
 console.log(sobrenome)
 console.log(saudacao)
+console.log(nomecompleto)
+console.log(nome, sobrenome + "!")
