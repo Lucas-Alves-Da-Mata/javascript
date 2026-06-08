09d4730c1cbb17ae667b8b9b0ad3032e89c645b4
julia vic <lucas.mata.dev@gmail.com>
2026-06-08 07:56:29 -0400

    add operador de adiçao

---
+++ operador-adicao-strings.js
@@ -0,0 +1,4 @@
+let nome = "lucas"
+let sobrenome = "alves"
+let nomecompleto = nome + " " + sobrenome;
+console.log(nomecompleto); // saida "lucas alves"
---
+++ tipos-de- dados-object.js
@@ -0,0 +1,9 @@
+let pessoa = {
+    nome: "lucas",
+    idade: 17,
+    solteiro: true,
+};
+// acessando propriedade do objeto
+console.log(pessoa.nome); // "lucas"
+console.log(pessoa.idade); // 17
+console.log(pessoa.solteiro); // true
